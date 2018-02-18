8bc32fefe647adfc27e7b6cb192b875b457253eb
heyfey <tsungtsohsieh@gmail.com>
2018-02-18 22:51:14 +0800

    debugger.load(): CreateProcess

--- chapter3/my_debugger.py
+++ chapter3/my_debugger.py
@@ -5,13 +5,50 @@ kernel32 = windll.kernel32
 
 class debugger():
     def __init__(self):
-        print("init")
-        # pass
+        pass
     
     def load(self, path_to_exe):
-        print("load")
-        # pass
+        print("load : " + path_to_exe)
         
+        # deCreation flag determines how to create the process
+        # set creation_flags = CREAT_NEW_CONSOLE if you want
+        # to see the calculator GUI
+        creation_flags = DEBUG_PROCESS
+        
+        # instantiate the structs
+        startupinfo = STARTUPINFO()
+        process_information = PROCESS_INFORMATION()
+        
+        # The following two options allow the started process
+        # to be shown as a separate window. This also illustrates
+        # how different settings in the STARTUPINFO struct can affect
+        # the debuggee.
+        startupinfo.dwFlags = 0x1
+        startupinfo.wShowWindow = 0x0
+        
+        # We then initialize the cb variable in STARTUPINFO struct
+        # which is just the size of the struct itself
+        startupinfo.cb = sizeof(startupinfo)
+        
+        ## Got an error using CreateProcessA() like the book.
+        ## Try out CreateProcessW().
+        if kernel32.CreateProcessW(path_to_exe,
+                                   None,
+                                   None,
+                                   None,
+                                   None,
+                                   creation_flags,
+                                   None,
+                                   None,
+                                   byref(startupinfo),
+                                   byref(process_information)):
+            
+            print("[*] We have successfully launched the process!")
+            print("[*] PID: %d" % process_information.dwProcessId)
+            
+        else:
+            print("[*] Error: 0x%08x." % kernel32.GetLastError())
+                  
         
 if __name__ == '__main__':
     debugger = debugger()
