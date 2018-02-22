5636688807134b6efddef26f3dbfe9a01573039b
heyfey <tsungtsohsieh@gmail.com>
2018-02-23 01:54:27 +0800

    Add Hardware Breakpoint

--- chapter3/my_test.py
+++ chapter3/my_test.py
@@ -1,4 +1,5 @@
 import my_debugger
+from my_debugger_defines import *
 
 debugger = my_debugger.debugger()
 
@@ -8,10 +9,9 @@ pid = input("Enter the PID of the process to attach to "
 debugger.attach(int(pid))
 
 printf_address = debugger.func_resolve(b"msvcrt.dll", b"printf")
-
 print("[*] Address of printf: 0x%08x" % printf_address)
 
-debugger.bp_set(printf_address)
+debugger.bp_set_hw(printf_address, 1, HW_EXECUTE)
 
 debugger.run()
 
